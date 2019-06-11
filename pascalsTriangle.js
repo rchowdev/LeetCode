632829297763ca6d0762930d7a6886eaebb8a6b9
rchow1944 <rchow2@buffalo.edu>
2019-06-11 14:34:05 -0400

    pascal triangle solution

---
+++ pascalsTriangle.js
@@ -0,0 +1,21 @@
+/*
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = (numRows) => {
+    let pascalTriangle = [];
+
+    for (let i = 0; i < numRows; i++) {
+        pascalTriangle[i] = new Array(i+1);
+
+        for (let j = 0; j < i+1; j++) {
+          if (j === 0 || j === i) {
+            pascalTriangle[i][j] = 1;
+          } else {
+            pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+          }
+        }
+    }
+
+    return pascalTriangle;
+};
